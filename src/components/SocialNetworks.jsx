0744fc2c795b3056918ab74a29a28c1aa1194bc9
Francisco Jr <juniorlina872@gmail.com>
2022-08-03 01:45:25 -0300

    SocialNetwork

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -1,10 +1,10 @@
 import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
-
+//import Link from 'react-link';
 import '../styles/components/socialnetworks.sass';
 
 const socialNetworks = [
-  { name: "linkedin",  icon: <FaLinkedinIn /> },
-  { name: "github",   icon: <FaGithub /> },
+  { name: "linkedin",  icon: <a href="https://www.linkedin.com/in/francisco-j%C3%BAnior-4b4a02119/" target="_blank"> <FaLinkedinIn /></a>},
+  { name: "github",   icon: <a href="https://github.com/Fjunior08?tab=repositories" target="_blank"><FaGithub /></a> },
   { name: "instagram", icon: <FaInstagram /> },
 ];
 
@@ -12,7 +12,7 @@ const SocialNetworks = () => {
   return (
   <section id="social-networks"> 
   {socialNetworks.map((network) => (
-  <a href="#" className="social-btn" id={network.name} key={network.name}>
+  <a href='Link' className="social-btn" id={network.name} key={network.name}>
       {network.icon}
   </a>
   ))}
