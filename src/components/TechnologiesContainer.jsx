deb69d661591d7c68f98d8f55468322fc5da2842
Francisco Jr <juniorlina872@gmail.com>
2022-08-03 21:42:35 -0300

    maincontent

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -11,12 +11,12 @@ import {
 import '../styles/components/technologiescontainer.sass';
 
 const technologies = [
-  { id: "html",  name: "HTML5",   desc: "Conhecimento avançado HTML5",    icon: <DiHtml5 /> },
-  { id: "css3",  name: "CSS3",    desc: "Conhecimento avançado CSS3",     icon: <DiCss3 /> },
-  { id: "js",    name: "JavaScript", desc: "Conhecimento avançado JavaScript", icon: <DiJsBadge /> },
-  { id: "node",  name: "Node.js", desc: "Conhecimento avançado Node.js",  icon: <DiNodejsSmall /> },
-  { id: "mysql", name: "MySQL",   desc: "Conhecimento avançado MySQL",    icon: <DiMysql /> },
-  { id: "react", name: "React",   desc: "Conhecimento avançado React.js", icon: <DiReact /> },
+  { id: "html",  name: "HTML5",      desc: "Estrutura de layouts, principais tags, HTML semântico, formulários.",         icon: <DiHtml5 /> },
+  { id: "css3",  name: "CSS3",       desc: "Estilização de layout, animação, responsiveis",                               icon: <DiCss3 /> },
+  { id: "js",    name: "JavaScript", desc: "Manipulação do DOM, tipos de dados, funções, array, objetos, eventos",        icon: <DiJsBadge /> },
+  { id: "node",  name: "Node.js",    desc: "Domínio, Aplicação, APIs, CRUD, funções, array, objetos, Express, Arquitetura MVC", icon: <DiNodejsSmall /> },
+  { id: "mysql", name: "MySQL",      desc: "Estruturas de dados, Viewer, Procedures, funções, T-SQL",                     icon: <DiMysql /> },
+  { id: "react", name: "React",      desc: "UseState, Props, funções, array, objetos, eventos, hooks",                    icon: <DiReact /> },
 ]
 
 const TechnologiesContainer = () => {
@@ -29,7 +29,7 @@ const TechnologiesContainer = () => {
           {tech.icon}
           <div className="technology-info">
           <h3>{tech.name}</h3>
-          <h4>{tech.desc}</h4>
+          <h5>{tech.desc}</h5>
           </div>
         </div>
       ))}
