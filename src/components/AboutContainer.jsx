07b01c4df3c1c919ef36e14a10caf7146a54a64b
Francisco Jr <juniorlina872@gmail.com>
2022-08-03 22:51:22 -0300

    Tech

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,7 +1,7 @@
 const AboutContainer = () => {
   return ( 
     <section className="about-container">
-      <h2>Sobre</h2>
+      <h2>Sobre mim</h2>
       <p>Desenvolvedor Front-End Junior com experiência em programação web, testes unitarios, APIs, 
         integração com usuarios e exelentes habilidades de comunicação, conhecimento em sistemas,
         competitivo e totalmente centrado.
--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,12 +1,7 @@
 const ProjectsContainer = () => {
   return (
     <section className="projects-container">
-      <h2>Projetos</h2>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae sed soluta incidunt rem eos nobis
-        porro voluptate? Pariatur et iste vel accusamus velit laudantium? Veniam doloribus repellat sequi
-        maxime deserunt.
-      </p>
+      <h2>Projetos</h2>     
       <a href="https://github.com/Fjunior08?tab=repositories" target="blank" className="btn">
         Ver Projetos
       </a>
